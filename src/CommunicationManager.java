0e74a2bee75fa849a8b04bf4554e9474d2c24f14
Mitchell Gray <meg346@cornell.edu>
2023-12-27 16:27:30 -0500

    Update

--- src/CommunicationManager.java
+++ src/CommunicationManager.java
@@ -1,45 +1,10 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class Client {
-
-	private static final int PORT = 1234;
-	private volatile static boolean errorFound = false;
-
-	public static void main(String[] args) {
-		try (BufferedReader reader = new BufferedReader(new FileReader("ipaddress.txt"))) {
-			String IPADDRESS = reader.readLine(); // Reads the first line of the file
-			System.out.println("IP found: " + IPADDRESS);
-			Thread client1 = new Thread(() -> startClient(IPADDRESS, PORT, "Client 1"));
-			client1.start();
-			Thread client2 = new Thread(() -> startClient(IPADDRESS, PORT, "Client 2"));
-			client2.start();
-			Thread client3 = new Thread(() -> startClient(IPADDRESS, PORT, "Client 3"));
-			client3.start();
-			Thread client4 = new Thread(() -> startClient(IPADDRESS, PORT, "Client 4"));
-			client4.start();
-		} catch (IOException e) {
-			System.out.println("Error reading file: " + e.getMessage());
-			System.exit(1);
-		}
-	}
-
-	public static void main2(String[] args) {
-		try (BufferedReader reader = new BufferedReader(new FileReader("ipaddress.txt"))) {
-			String IPADDRESS = reader.readLine(); // Reads the first line of the file
-			System.out.println("IP found: " + IPADDRESS);
-			Thread client5 = new Thread(() -> startClient(IPADDRESS, PORT, "Client 5"));
-			client5.start();
-		} catch (IOException e) {
-			System.out.println("Error reading file: " + e.getMessage());
-			System.exit(1);
-		}
-	}
-
+public class CommunicationManager {
 	private static void startClient(String hostname, int port) {
 		while (true) {
 			System.out.println("Trying to connect to Server");
